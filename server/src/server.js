7981e908d323db270c406bb3eff9491812df5da9
Banto Klara <bantoklara2@gmail.com>
2025-09-14 14:37:28 +0300

    chore: add routes, controllers, services, and repository layers and test the db connection with sample data

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { PORT } from "./config/env.js";
+import router from "./routes/TestRouter.js";
 
 // testing server 
-
 const app = express();
+
+app.use(express.json());
+
+app.use('/api', router);
 
 app.listen(PORT, () => {
    console.log(`Server is running on http://localhost:${PORT}`);

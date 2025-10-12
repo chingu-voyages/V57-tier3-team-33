21ab9167376ada6ea2f1398ae560c255caa21001
Tibamwenda Anthony <132336450+AskTiba@users.noreply.github.com>
2025-10-12 13:53:38 +0300

    Update AuthLayout.tsx

--- client/src/pages/AuthLayout.tsx
+++ client/src/pages/AuthLayout.tsx
@@ -1,7 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
 export default function AuthLayout() {
+  const { user } = useAuth();
+
   return (
-    <Outlet />
+    <>
+      {
+        user
+          ? <Navigate to="/" />
+          : <Outlet />
+      }
+    </>
   );
 }

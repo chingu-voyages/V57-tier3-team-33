96ae20ff4fcd992fddaff20cf33e15d9ab17393c
Tibamwenda Anthony <132336450+AskTiba@users.noreply.github.com>
2025-10-12 13:53:38 +0300

    Update AuthLayout.tsx

--- client/src/pages/Auth.tsx
+++ client/src/pages/Auth.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { PBLogo } from "../components/brand";
 import GitHub from "../components/icons/github";
 import { useAuth } from "../context/AuthContext";
@@ -13,14 +14,18 @@ export default function Auth() {
     <main className="grid grid-cols-1 md:grid-cols-2">
       <div className="h-screen relative hidden md:block">
         <div className="absolute top-4 left-4">
-          <PBLogo width={200} />
+          <Link to="/">
+            <PBLogo width={200} />
+          </Link>
         </div>
         <img src="/pbImg.png" alt="" className="w-full h-full object-cover" />
       </div>
       <div className="flex flex-col justify-center items-center h-screen bg-gray-50 px-4">
         <div className="flex flex-col justify-center items-center text-center">
           <div className="md:hidden mb-8">
-            <PBLogo width={150} />
+            <Link to="/">
+              <PBLogo width={150} />
+            </Link>
           </div>
           <h2 className="text-3xl md:text-4xl font-bold mb-4">Thanks for trying PullBoard</h2>
           <h3 className="text-lg text-gray-600 mb-8">Login to start tracking your pull requests</h3>
